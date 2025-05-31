f27e560c4d8149ec8be04a6739332642298589c9
PranitaPaudel <pranitapaudel67@gmail.com>
2025-05-31 16:37:18 +0545

    feat: add login link in register component

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import "./loginregister.css";
 
-function register() {
+function Register() {
   const [email, setEmail] = useState("");
   const [uname, setUname] = useState("");
   const [password, setPassword] = useState("");
@@ -10,31 +11,36 @@ function register() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Signup</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Username"
-        value={uname}
-        onChange={(e) => setUname(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <button type="submit">Signup</button>
-    </form>
+    <div className="container">
+      <form className="box" onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          value={uname}
+          onChange={(e) => setUname(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Register</button>
+        <div className="link">
+          Already have an account? <a href="/login">Login</a>
+        </div>
+      </form>
+    </div>
   );
 }
-export default register;
+export default Register;
