5cc91568ed46fdc5e735679c646c1b85c07275b2
PranitaPaudel <pranitapaudel67@gmail.com>
2025-06-03 13:43:33 +0545

    feat: add validation for input fields

--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -1,16 +1,58 @@
 import "../index.css";
-import { Fragment } from "react";
-
+import { Fragment, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const [uname, setUname] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateEmail = (email: string): boolean => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  };
+
+  const validateUsername = (username: string): boolean => {
+    const regex = /^[A-Za-z][A-Za-z0-9_]*$/;
+    return regex.test(username);
+  };
+
+  const validatePassword = (password: string): boolean => {
+    return password.length >= 8;
+  };
+
+  const handldeSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      toast.error("Invalid email format.");
+      return;
+    }
+
+    if (!validateUsername(uname)) {
+      toast.error(
+        "Username must start with a letter and can contain only letters, numbers, and underscores."
+      );
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      toast.error("Password must be at least 8 characters long.");
+      return;
+    }
+
+    toast.success("Registered Successfully!");
+  };
   return (
     <Fragment>
+      <ToastContainer position="top-center" />
+
       <div className="min-h-screen flex items-center justify-center bg-gray-100">
         <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-2xl shadow-md">
           <h2 className="text-xl text-center font-bold text-gray-600">
             Register
           </h2>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handldeSubmit} method="">
             <div>
               <label
                 htmlFor="email"
@@ -19,10 +61,13 @@ function Register() {
                 Email:
               </label>
               <input
-                type="text"
+                type="email"
                 id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Enter email"
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
+                required
               />
             </div>
             <div>
@@ -35,8 +80,11 @@ function Register() {
               <input
                 type="text"
                 id="uname"
+                value={uname}
+                onChange={(e) => setUname(e.target.value)}
                 placeholder="Enter username"
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
+                required
               />
             </div>
             <div>
@@ -49,8 +97,11 @@ function Register() {
               <input
                 type="password"
                 id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 placeholder="Enter password"
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
+                required
               />
             </div>
             <button
