8d6cf4dbf1ad1ff29b6e21780dd5635bd035e631
PranitaPaudel <pranitapaudel67@gmail.com>
2025-06-03 12:26:35 +0545

    chore: add routes for login and register form with tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Register from "./components/Register";
+import Register from "./components/register";
 import Login from "./components/Login";
+import Logintw from "./components/Logintw";
+import Registertw from "./components/Registertw";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/logintw" element={<Logintw />} />
+      <Route path="/registertw" element={<Registertw />} />
     </Routes>
   );
 }
