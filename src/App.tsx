0511723848e98cb636c5777e0ee5ea853d0e897d
PranitaPaudel <pranitapaudel67@gmail.com>
2025-06-03 12:56:29 +0545

    chore: add routing for login and register component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>Todo App</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
 export default App;
